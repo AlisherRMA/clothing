55e9b34217cafd508f4cd3af6a1570ff0998cfb8
Alisher <ashimkhan@yandex.kz>
2021-11-14 21:56:08 +0600

    feat: added wrapper header component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="shop" element={<ShopPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shop" element={<ShopPage />} />
+      </Routes>
+    </div>
   );
 }
 
