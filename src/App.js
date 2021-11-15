7ceb37841bb2d819a2c50c705a91ab4694c72a7f
Alisher <ashimkhan@yandex.kz>
2021-11-15 20:43:35 +0600

    feat: added auth page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import AuthPage from "./pages/auth/auth.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="shop" element={<ShopPage />} />
+        <Route path="signin" element={<AuthPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./auth.styles.scss";
+
+import SignIn from "../../components/auth/sign-in/sign-in.component";
+
+const AuthPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default AuthPage;
