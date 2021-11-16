3d14710a013f9a1cce03ddda2a15d033c10a40fb
Alisher <ashimkhan@yandex.kz>
2021-11-16 23:55:30 +0600

    implemented firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
   measurementId: "G-4WSB2K9L1X",
 };
 
+export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    console.log({ userAuth });
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    console.log({ additionalData });
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./auth.styles.scss";
 
 import SignIn from "../../components/auth/sign-in/sign-in.component";
+import SignUp from "../../components/auth/sign-up/sign-up.component";
 
 const AuthPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
