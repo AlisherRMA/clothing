ce526ad6f4fb7ee2ee779db1955ac8c6cbe25ff8
Alisher <ashimkhan@yandex.kz>
2021-11-20 14:40:02 +0600

    feat: redirect user from auth page if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { Routes } from "react-router";
-import { Route, Routes } from "react-router-dom";
+// import { redirect } from "react-router";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
@@ -43,15 +43,19 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
-          <Route path="signin" element={<AuthPage />} />
+          <Route path="signin" element={this.props.currentUser ? <Navigate replace to="/" /> : <AuthPage />} />
         </Routes>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
