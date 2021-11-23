c7f148e581226bb896cd78bc99412ee6c59f79df
Alisher <ashimkhan@yandex.kz>
2021-11-23 21:44:09 +0600

    feat(checkout): clear item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
+import { removeItem } from "./cart.actions";
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,6 +18,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      console.log("reducer", action.payload);
+      return {
+        ...state,
+        cartItems: removeItem(state.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     default:
       return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,13 +9,29 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 };
 
 export const getCartItemsQuantity = (cartItems) => {
+  console.log({ cartItems });
+  if (!cartItems || !cartItems.length) return 555;
   return cartItems.reduce((prev, curr) => {
     return prev + curr.quantity;
   }, 0);
 };
 
 export const getCartItemsTotalPrice = (cartItems) => {
+  if (!cartItems || !cartItems.length) return 9999;
   return cartItems.reduce((prev, curr) => {
     return prev + curr.quantity * curr.price;
   }, 0);
+};
+
+export const removeItemFromCart = (cartItems, item) => {
+  return cartItems.filter((cartItem) => cartItem.id !== item.id);
+};
+
+export const removeItem = (cartItems, cartItemToRemove) => {
+  return cartItems;
+  // const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  // if (existingCartItem.quantity === 1) return removeItemFromCart(cartItems, cartItemToRemove);
+
+  // return cartItems.map((cartItem) => (cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem));
 };
